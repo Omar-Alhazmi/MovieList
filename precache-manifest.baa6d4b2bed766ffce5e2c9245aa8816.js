c506b73e3f32c7305da7d59237d5f731e9f508cb
vex-xcc <vex.xcc@gmail.com>
2020-02-26 16:05:44 +0300

    Updates

---
+++ precache-manifest.baa6d4b2bed766ffce5e2c9245aa8816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3762d171db408f4076009b70ec6f242",
+    "url": "/MovieList/index.html"
+  },
+  {
+    "revision": "8fff3030d48d35effb34",
+    "url": "/MovieList/static/css/2.3deb16ae.chunk.css"
+  },
+  {
+    "revision": "761cb72b6c86ada67b2d",
+    "url": "/MovieList/static/css/main.6bd785c3.chunk.css"
+  },
+  {
+    "revision": "8fff3030d48d35effb34",
+    "url": "/MovieList/static/js/2.969a9e86.chunk.js"
+  },
+  {
+    "revision": "171b1e24804ae734c94e8c22b1926ebb",
+    "url": "/MovieList/static/js/2.969a9e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761cb72b6c86ada67b2d",
+    "url": "/MovieList/static/js/main.a0f315e3.chunk.js"
+  },
+  {
+    "revision": "a1e1a316ab55c082d16f",
+    "url": "/MovieList/static/js/runtime-main.38dd6f26.js"
+  }
+]);
